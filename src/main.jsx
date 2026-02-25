131776cb4c12c4f05c5af0591a71ca57692802cd
php2700 <php2.dbvertex@gmail.com>
2026-02-25 19:36:52 +0530

    login

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// 1. Yahan apni ID ko quotes (" ") ke andar likhein
+const GOOGLE_CLIENT_ID = "789826047913-gfh69395p2k3udh01uvveascm7oapkis.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2. clientId mein wahi constant variable pass karein */}
+<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+  <App />
+</GoogleOAuthProvider>
   </StrictMode>,
 )
